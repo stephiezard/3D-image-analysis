849cafd69bcedc3fc8caec43a5e480e3be32a542
Stephanie Lipiainen <stephanielipiainen@Stephanies-MacBook-Air-5.local>
2025-07-30 12:49:08 +0300

    projection of gray channel

--- ims_reader.py
+++ ims_reader.py
@@ -1,6 +1,9 @@
 import numpy as np
 from imaris_ims_file_reader.ims import ims
 import napari
+import cv2
+
+run_visualization = False
 
 # Load .ims file
 file_path = "TESTI.ims"
@@ -13,19 +16,25 @@ t = 0
 # Extract 4 channels as 3D volumes (Z, Y, X)
 channels = [np.array(data[t, c, :, :, :]) for c in range(data.shape[1])]
 
-# Launch Napari viewer
-viewer = napari.Viewer()
+if run_visualization:
+    # Launch Napari viewer
+    viewer = napari.Viewer()
 
-# Add each channel as a separate grayscale 3D volume
-for idx, channel_data in enumerate(channels):
-    viewer.add_image(
-        channel_data,
-        name=f"Channel {idx}",
-        colormap="gray",
-        blending="additive",  # you can try 'translucent' too
-        rendering="mip",      # maximum intensity projection (3D)
-    )
+    # Add each channel as a separate grayscale 3D volume
+    for idx, channel_data in enumerate(channels):
+        viewer.add_image(
+            channel_data,
+            name=f"Channel {idx}",
+            colormap="gray",
+            blending="additive",  # you can try 'translucent' too
+            rendering="mip",      # maximum intensity projection (3D)
+        )
 
-napari.run()
+    napari.run()
 
+gray = np.array(data[0, 0, :,:,:]).squeeze()
+gray = gray.astype(np.float32)/2**16
+gray_sum = gray.sum(axis=0)
+cv2.imshow("gray_projection", gray_sum)
+k = cv2.waitKey(0)
 
